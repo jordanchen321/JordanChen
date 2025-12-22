1a9610cb38d4b1e18fea47f6302dd5949149f704
Jordan Chen <140654452+jordanchen321@users.noreply.github.com>
2025-12-21 20:32:59 -0800

    Simplify next.config.js by removing image settings
    Removed unnecessary image configuration from nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", 
-
-  images: {
-    unoptimized: true, // only if you had image issues
-  }
+  output: "export"
 };
 
 export default nextConfig;
+
 
 
 
