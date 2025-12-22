724790969eadb72475fea2f45261d72a6dc0c63c
Jordan Chen <140654452+jordanchen321@users.noreply.github.com>
2025-12-21 20:26:42 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export", 
+
+  images: {
+    unoptimized: true, // only if you had image issues
+  }
 };
 
 export default nextConfig;
+
 
 
